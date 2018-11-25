9c9128daf5cabaa8d490967ea87b5925a3f3d198
Zhiyuan <jerryz.cheng@mail.utoronto.ca>
2018-11-25 17:07:56 -0500

    add matlab function for SIFT detection

---
+++ read_images.m
@@ -0,0 +1,21 @@
+function read_images(path)
+    dirlist = dir(path);
+    for i = 1:length(dirlist)
+        if length(dirlist(i).name) <= 4
+            continue;
+        end
+        if strcmp(dirlist(i).name(end-3:end), ".jpg")
+            img_path = strcat(path, dirlist(i).name);
+            disp(img_path)
+            img = imread(img_path);
+            img = single(rgb2gray(img));
+            [f, d] = vl_sift(img);
+            if size(f, 2) == 0
+                continue;
+            end
+            data_path = strcat('.\sift_data\' ,dirlist(i).name(1:end-3), 'csv');
+            dlmwrite(data_path, f);
+            dlmwrite(data_path, d, '-append');
+        end
+    end
+end
